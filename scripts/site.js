066a6ab69412f831e1140f0af4ab344089f306cc
Lisa Spell <mjspell@gmail.com>
2017-05-11 14:28:13 -0400

    push it

--- scripts/site.js
+++ scripts/site.js
@@ -9,3 +9,25 @@ navElement.addEventListener("transitionend", function(e) {
 	if(e.propertyName == "top")
         navElement.classList.toggle("nav-open");
 });
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                var found = allText.indexOf('data-schedule="next show"');
+
+                console.log(found);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("schedule.html")
